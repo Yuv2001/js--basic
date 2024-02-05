d1ad76b3afa3c7f1edac217b0403a56978d54088
Yuvraj Gupta <115615702+Yuv2001@users.noreply.github.com>
2024-02-05 17:10:58 +0000

    type conversion

---
+++ 01-basic/03_conversion.js
@@ -0,0 +1,26 @@
+let Score ="yuvraj"
+
+console.log(typeof Score);
+console.log(typeof(Score));
+
+let valueInNumaber= Number(Score)
+console.log(valueInNumaber);
+console.log(typeof valueInNumaber);
+
+//"33" = 33
+//"33abc" = NaN
+//true = 1, false = 0
+
+let IsLogIn = "yuvraj"
+let booleanislogIn = Boolean(IsLogIn)
+
+console.log(booleanislogIn);
+// 1= true; 0= false
+//"" = false
+//"yuvraj"= true
+
+let SomeNumber=33
+let StringNumber= String(SomeNumber)
+
+console.log(typeof StringNumber);
+console.log(StringNumber);
