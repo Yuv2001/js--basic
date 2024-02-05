a83070f87d270bd97b56a15b08714e57bc44110e
Yuvraj Gupta <115615702+Yuv2001@users.noreply.github.com>
2024-02-05 08:31:22 +0000

    container cofig done

--- 01-basic/01_variable.js
+++ 01-basic/01_variable.js
@@ -3,6 +3,16 @@ let accountEmail="yuvraj@gmail.com"
 var accountPassword="123"
 accountCity="delhi"
 
-//accountId=2
+//accountId=2 // not allowed
+accountEmail="rc@hc.com"
+accountPassword="3652"
+accountCity= "goa"
+let accountState;
 
 console.log(accountId);
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+console.table([accountId, accountEmail,accountPassword,accountCity,accountState])
