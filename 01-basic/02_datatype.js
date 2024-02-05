77d47c5f627f77a809f3c5df5d9b19224af05f09
Yuvraj Gupta <115615702+Yuv2001@users.noreply.github.com>
2024-02-05 09:09:51 +0000

    data type

---
+++ 01-basic/02_datatype.js
@@ -0,0 +1,27 @@
+"use strict" // treat all code  newer version of javascript
+
+//alert(4+5) // we using nodejs ,not brower
+
+console.log(4
+    +
+    5);// code readability should be high
+
+console.log("yuvraj");
+
+let name = "yuvraj"
+let age = 23
+let isloggedIn = false
+let state;
+
+// number => 2 ki pwer 53
+//bigint 
+//string =>""
+// boolean => true/false
+//null => sandlone
+//undefined
+// symbol = unique
+
+// object
+
+console.log(typeof null);// object
+console.log(typeof undefined);// undefined
