6bb7af65b1dbc256e8457bd0c0ada46bdc94127c
Yuvraj Gupta <115615702+Yuv2001@users.noreply.github.com>
2024-02-05 18:22:41 +0000

    data conversion

--- 01-basic/03_conversion.js
+++ 01-basic/03_conversion.js
@@ -24,3 +24,41 @@ let StringNumber= String(SomeNumber)
 
 console.log(typeof StringNumber);
 console.log(StringNumber);
+
+
+//**************************************Operations**************************************//
+
+let value = 3
+let NegValue =-value
+console.log(NegValue);
+
+console.log(2+9);
+console.log(2-9);
+console.log(2*9);
+console.log(2**9);
+console.log(2%9);
+
+
+let str1 = "hii"
+let str2 ="yuvraj"
+let str3=str1 + str2
+console.log(str3);
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
