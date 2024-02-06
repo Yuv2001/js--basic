012795db0c22b07a2bd2aa74926246e51e2353ca
Yuvraj Gupta <115615702+Yuv2001@users.noreply.github.com>
2024-02-06 18:36:17 +0000

    sumary

---
+++ 01-basic/datatypesumary.js
@@ -0,0 +1,37 @@
+// premitive
+
+// 7 types: String , Number , boolean, null, undefined, Symbol, BigInt
+
+const score =100
+const scoreValue =100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id=== anotherId);
+
+//const bigNumber = 56846644654989654n
+
+//Refrence (Non primitive)
+
+// Array , Object, Functions
+
+
+const heros =["iron","doctor","thor"];
+
+let myObj ={
+    name: "yuvraj",
+    age: 23,
+}
+
+
+const myFunction = function(){
+    console.log("my world");
+}
+
+
+console.log(typeof bigNumber);
