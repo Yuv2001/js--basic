5423f7caa3aeb9ee23a6788e0ba43ddb62b60feb
Yuvraj Gupta <115615702+Yuv2001@users.noreply.github.com>
2024-02-11 18:20:13 +0000

    update string

--- 01-basic/05_string.js
+++ 01-basic/05_string.js
@@ -3,3 +3,9 @@ const repocount= 50
 console.log(name + repocount+ "value");
 
 console.log(`my name is ${name} and repocount is ${repocount} `);
+
+const gamName = new String('yuvraj')
+
+console.log(gamName[0]);
+console.log(gamName.toUpperCase());
+console.log(gamName.charAt(3));
